bf6dd7b0c48d1b9191bda4dd15640699879b9462
vrusso <vincentrusso1@gmail.com>
2024-02-24 08:05:53 -0500

    Adding hardware run script.

---
+++ notebooks/hardware_run.py
@@ -0,0 +1,378 @@
+import numpy as np
+import os
+import pickle
+from enum import Enum
+
+import qiskit
+
+from qiskit_aer.noise import NoiseModel, pauli_error
+from qiskit.providers import Backend
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+
+DATA_PATH = os.path.join("..", "data")
+
+def bitflip_model(p):
+    # Example error probabilities
+    p_meas = p
+    p_gate1 = p
+
+    # QuantumError objects
+    error_meas = pauli_error([('X',p_meas), ('I', 1 - p_meas)])
+    error_gate1 = pauli_error([('X',p_gate1), ('I', 1 - p_gate1)])
+    error_gate2 = error_gate1.tensor(error_gate1)
+
+    # Add errors to noise model
+    noise_bit_flip = NoiseModel()
+    noise_bit_flip.add_all_qubit_quantum_error(error_meas, "measure")
+    noise_bit_flip.add_all_qubit_quantum_error(error_gate1, ["u1", "u2", "u3"])
+    noise_bit_flip.add_all_qubit_quantum_error(error_gate2, ["cx"])
+
+    return noise_bit_flip
+
+# Settings for extended Wigner's friend scenario.
+class Setting(Enum):
+    PEEK = 1
+    REVERSE_1 = 2
+    REVERSE_2 = 3
+
+# Observers for scenario are Alice and Bob.
+class Observer(Enum):
+    ALICE = 0
+    BOB = 1
+
+
+# Experiment settings (peek, reverse_1, and reverse_2).
+PEEK = Setting.PEEK.value
+REVERSE_1 = Setting.REVERSE_1.value
+REVERSE_2 = Setting.REVERSE_2.value
+SETTINGS = [PEEK, REVERSE_1, REVERSE_2]
+
+# "Super"-observers (Alice and Bob).
+ALICE = Observer.ALICE.value
+BOB = Observer.BOB.value
+OBSERVERS = [ALICE, BOB]
+
+# Angles and beta term used for Alice and Bob measurement operators from arXiv:1907.05607.
+# Note that despite the fact that degrees are used, we need to convert this to radians.
+ANGLES = {PEEK: np.deg2rad(168), REVERSE_1: np.deg2rad(0), REVERSE_2: np.deg2rad(118)}
+BETA = np.deg2rad(175)
+
+def decode_results(results: dict[str, float], charlie_size: int, debbie_size: int) -> dict[str, float]:
+    """Take majority vote of measurement bit-strings."""
+    decoded_results = {}
+
+    # For each setting, there is a dictionary of measurement results.
+    for setting in results:
+        if setting == (PEEK, REVERSE_1) or setting == (PEEK, REVERSE_2):
+            # Debbie's size is 1 because no PEEK setting
+            debbie_size = 1
+            
+            setting_results = {}
+            # Decode the keys for each measurement result of the setting.
+            for k, v in results[setting].items():
+                alice_friend, bob_friend = k[:charlie_size], k[-debbie_size:]
+
+                alice_zero_count, bob_zero_count = alice_friend.count("0"), bob_friend.count("0")
+   
+                alice_decoding = "0" if alice_zero_count >= charlie_size // 2 + 1 else "1"
+                bob_decoding = "0" if bob_zero_count >= 1 else "1"
+
+                if alice_decoding + bob_decoding in setting_results.keys():
+                    setting_results[alice_decoding + bob_decoding] += v
+                else:
+                    setting_results[alice_decoding + bob_decoding] = v
+            decoded_results[setting] = setting_results
+        else:
+            decoded_results[setting] = results[setting]
+            
+    return decoded_resGults
+
+
+def double_expect(settings: list[Setting], results: dict) -> float:
+    """Expectation value of product of two operators."""
+    probs = results[settings]
+    # <AB> = P(00) - P(01) - P(10) + P(11)
+    return (
+        probs.get("00", 0) 
+        - probs.get("01", 0) 
+        - probs.get("10", 0) 
+        + probs.get("11", 0)
+    )
+
+
+def generate_all_experiments(
+    backend: Backend,
+    noise_model: NoiseModel,
+    shots: float,
+    angles: list[float],
+    beta: float,
+    charlie_size: int,
+    debbie_size: int,
+) -> dict[tuple[Observer, Observer], list[float]]:
+    """Generate probabilitites for all combinations of experimental settings."""
+    all_experiment_combos = [[PEEK, REVERSE_1], [PEEK, REVERSE_2], [REVERSE_2, REVERSE_1], [REVERSE_2, REVERSE_2]]
+    
+    results = {}
+    for alice, bob in all_experiment_combos:
+        ewfs_circuit = ewfs(alice, bob, angles, beta, charlie_size, debbie_size)
+
+        job = qiskit.execute(
+            experiments=ewfs_circuit,
+            backend=backend,
+            noise_model=noise_model,
+            basis_gates=noise_model.basis_gates if noise_model is not None else None,
+            shots=shots,
+        )
+        counts = job.result().get_counts()
+        
+        # Convert counts to probabilities.
+        probabilities = {key[::-1]: value / shots for key, value in counts.items()}
+
+        results[(alice, bob)] = probabilities
+    return results
+
+
+def prepare_bipartite_system(qc: QuantumCircuit):
+    """Generates the state: 1/sqrt(2) * (|01> - |10>)"""
+    qc.x(ALICE)
+    qc.x(BOB)
+    qc.h(ALICE)
+    qc.cx(ALICE, BOB)
+
+
+def cnot_ladder(qc: QuantumCircuit, observer: Observer, friend_qubit: int, friend_size: int, reverse: bool, internal_copy: bool):
+    """CNOT ladder circuit (GHZ without Hadamard)."""
+    if internal_copy:
+        if reverse:
+            for i in range(friend_size-1):
+                qc.cx(friend_qubit + friend_size-2-i, friend_qubit+friend_size-1-i)
+            qc.cx(observer, friend_qubit)
+        else:
+            qc.cx(observer, friend_qubit)
+            for i in range(friend_size-1):
+                qc.cx(friend_qubit+i, friend_qubit + i + 1)
+    else:
+        if reverse:
+            for i in range(friend_size):
+                qc.cx(observer, friend_qubit+friend_size-1-i)
+        else:
+            for i in range(friend_size):
+                qc.cx(observer, friend_qubit + i)
+
+
+def ewfs_rotation(qc: QuantumCircuit, qubit: int, angle: float):
+    qc.rz(-angle, qubit)
+    qc.h(qubit)
+    
+def apply_setting(qc: QuantumCircuit, 
+                  observer: Observer, 
+                  setting: Setting, 
+                  angle: float, 
+                  observer_creg: list[int] | int, 
+                  friend_qubits: int, 
+                  friend_size: int):
+    """Apply either the PEEK or REVERSE_1/REVERSE_2 settings."""    
+    if setting is PEEK:
+        # Ask friend for the outcome.
+        qc.measure(friend_qubits, observer_creg)
+
+    elif setting in [REVERSE_1, REVERSE_2]:
+        cnot_ladder(qc, observer, friend_qubits[0], friend_size, reverse=True, internal_copy=True)
+
+        # For either REVERSE_1 or REVERSE_2, apply the appropriate angle rotations.
+        # Note that in this case, the rotation should occur on the observer's qubit.
+        if observer is ALICE:
+            qc.h(ALICE)
+            qc.rz(ANGLES[1], ALICE)        
+
+        if observer is BOB:
+            qc.h(BOB)        
+            qc.rz((BETA - ANGLES[1]), BOB)        
+        ewfs_rotation(qc, observer, angle)            
+        qc.measure(observer, observer_creg)
+        
+def ewfs(alice_setting: Setting, 
+        bob_setting: Setting, 
+        angles: list[float], 
+        beta: float,
+        charlie_size: int,
+        debbie_size: int) -> QuantumCircuit:
+    """Generate the circuit for extended Wigner's friend scenario."""    
+    # Define quantum registers
+    alice_size, bob_size = 1, 1
+    sys_size = alice_size + bob_size
+    meas_size = 2
+
+    alice, bob, charlie, debbie = [
+        QuantumRegister(size, name=name) 
+        for size, name in zip([alice_size, bob_size, charlie_size, debbie_size], 
+                              ["Alice", "Bob", "Charlie", "Debbie"])
+    ]    
+    if (alice_setting == PEEK and bob_setting != PEEK):
+        measurement = ClassicalRegister(charlie_size + 1, name="Measurement")
+        alice_creg = list(range(charlie_size))
+        bob_creg = charlie_size
+    else:
+        measurement = ClassicalRegister(meas_size, name="Measurement")
+        alice_creg = 0
+        bob_creg = 1
+        
+    # Create the Quantum Circuit with the defined registers
+    qc = QuantumCircuit(alice, bob, charlie, debbie, measurement)
+
+    charlie_qubits = range(sys_size, (sys_size + charlie_size))
+    debbie_qubits = range(sys_size + charlie_size, sys_size + (charlie_size + debbie_size))
+    
+    # Prepare the bipartite quantum system
+    prepare_bipartite_system(qc)
+    
+    # Rotations for measurement.
+    ewfs_rotation(qc, ALICE, angles[1])
+    ewfs_rotation(qc, BOB, beta - angles[1])
+
+    # Apply the CNOT ladder for Alice-Charlie and Bob-Debbie
+    cnot_ladder(qc, ALICE, charlie_qubits[0], charlie_size, reverse=False, internal_copy=True)
+    cnot_ladder(qc, BOB, debbie_qubits[0], debbie_size, reverse=False, internal_copy=True)
+
+    # Apply the settings for Alice/Charlie and Bob/Debbie
+    apply_setting(qc, ALICE, alice_setting, angles[alice_setting], alice_creg, charlie_qubits, charlie_size)
+    apply_setting(qc, BOB, bob_setting, (beta - angles[bob_setting]), bob_creg, debbie_qubits, debbie_size)
+
+    return qc
+
+
+def save_data(
+    results: dict,
+    backend: Backend,
+    friend_sizes: int,
+    num_trials: int,
+    shots: int,
+):
+    qubits = max(friend_sizes)
+
+    output_file_name = f"{backend.name}_qubits_{qubits}_trials_{num_trials}_shots_{shots}.pickle"
+    output_path = os.path.join(DATA_PATH, output_file_name)
+
+    print(f"Writing data to: {output_path}")
+    with open(output_path, "wb") as handle:    
+        pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def compute_inequalities(results, verbose=False):
+    A1B2 = double_expect((PEEK, REVERSE_1), results)
+    A1B3 = double_expect((PEEK, REVERSE_2), results)
+
+    A3B2 = double_expect((REVERSE_2, REVERSE_1), results)
+    A3B3 = double_expect((REVERSE_2, REVERSE_2), results)
+    
+    # Eq. (18) from [1].
+    semi_brukner = -A1B2 + A1B3 - A3B2 - A3B3 - 2
+    
+    if verbose:
+        print(f"{semi_brukner=} -- is violated: {semi_brukner > 0}")
+        
+    return {"semi_brukner": semi_brukner}
+
+
+def run_experiment(
+    backend: Backend,
+    noise_model: NoiseModel,
+    friend_sizes: list[int],
+    shots: float,
+    num_trials: int = 1,
+    verbose: bool = False,
+    save: bool = False,
+) -> dict:
+    all_results = {
+        fs: {inequality: [] for inequality in ["semi_brukner"]}
+        for fs in friend_sizes
+    }
+
+    for friend_size in friend_sizes:
+        print(f"{friend_size=}")
+        for _ in range(num_trials):
+            results = generate_all_experiments(
+                backend=backend,
+                noise_model=noise_model,
+                shots=shots,
+                angles=ANGLES,
+                beta=BETA,
+                charlie_size=friend_size,
+                debbie_size=1,
+            )
+            violations = compute_inequalities(decode_results(results, charlie_size=friend_size, debbie_size=1), verbose=verbose)
+            for key in violations:
+                all_results[friend_size][key].append(violations[key])
+    if save:
+        save_data(results=results, backend=backend, friend_sizes=friend_sizes, num_trials=num_trials, shots=shots)
+    return all_results
+
+def plot_results(
+    ax,
+    results: dict, 
+    friend_sizes: list[int], 
+    plot_title: str,
+    plot_error_bars: bool = False, 
+):
+    # Compute averages and standard deviations
+    avg_results = {}
+    std_results = {}
+    for fs in results:
+        avg_results[fs] = {}
+        std_results[fs] = {}
+        for key in results[fs]:
+            avg_results[fs][key] = np.mean(results[fs][key])
+            if plot_error_bars:
+                std_results[fs][key] = np.std(results[fs][key])
+
+    # Plotting configurations
+    markers = ["o"]
+    colors = ["tab:blue", "tab:orange", "tab:green", "tab:red"]
+    titles = ["Semi-Brukner"]
+    marker_size = 10
+    line_width = 2.5
+
+    for i, key in enumerate(["semi_brukner"]):
+        means = [np.mean(results[fs][key]) for fs in friend_sizes]
+        errors = [np.std(results[fs][key]) for fs in friend_sizes] if plot_error_bars else None
+        ax.plot(
+            friend_sizes,
+            means,
+            label=titles[i],
+            marker=markers[i],
+            markersize=marker_size,
+            linestyle="-",
+            linewidth=line_width,
+            color=colors[i % len(colors)]
+        )
+        if plot_error_bars:
+            ax.errorbar(friend_sizes, means, yerr=errors, fmt="none", color=colors[i % len(colors)], capsize=5, elinewidth=line_width)
+
+    # Adjusted line labels with increased line width
+    ax.axhline(0, color="tab:red", linestyle="dashed", label="No violation", linewidth=line_width)
+    ax.axhline(0.380364, color="tab:green", linestyle="dashed", label="Theoretical maximum", linewidth=line_width)
+
+    ax.set_xticks(friend_sizes)
+    ax.set_title(plot_title)
+    ax.grid(True)
+
+
+# Parameters for all plots
+friend_sizes = range(1, 12)
+num_trials = 1
+shots = 10_000
+backend = qiskit.Aer.get_backend("aer_simulator")
+
+# Ideal simulator data
+noise_model = None
+simulator_results = run_experiment(
+    backend=backend,
+    noise_model=noise_model,
+    friend_sizes=friend_sizes,
+    num_trials=num_trials,
+    shots=shots,    
+    verbose=True,
+    save=True,
+)
+
+
+
