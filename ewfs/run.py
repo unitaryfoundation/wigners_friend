e7918619abac71c2d0feb6eafdcb0c91a2fc68a5
vprusso <vincentrusso1@gmail.com>
2024-12-26 12:51:53 -0500

    refactor: run now has the ability to support debbie size for > 1.

--- ewfs/run.py
+++ ewfs/run.py
@@ -15,7 +15,8 @@ DEFAULT_SETTINGS = [setting for setting in itertools.product(["peek", "reverse_1
 def run_experiment(
     shots: int = 10_000,
     num_trials: int = 2,
-    friend_sizes: range = range(1, 3),
+    charlie_sizes: range = range(1, 3),
+    debbie_sizes: range = range(1, 2),
     strategy: str = "random",
     backend: qiskit_aer.backends.aerbackend.AerBackend = qiskit_aer.Aer.get_backend("aer_simulator"),
     settings: list[tuple[str, str]] = DEFAULT_SETTINGS,
@@ -26,35 +27,39 @@ def run_experiment(
     tasks = defaultdict(dict)
 
     # Construct all circuits to be run over all qubit sizes.
-    for charlie_size in friend_sizes:
-        for trial in range(1, num_trials + 1):
-            circuits = {}
-        
-            # Create circuits for each EWFS setting.
-            # Note, we remove barriers in the Qiskit circuit.
-            for alice_setting, bob_setting in settings:
-                circuit = EWFS(
-                    alice_setting=alice_setting,
-                    bob_setting=bob_setting,
-                    strategy=strategy,
-                    charlie_size=charlie_size,
-                    debbie_size=1).circuit()
-                circuits[(alice_setting, bob_setting)] = circuit
-        
-            # Use backend to transpile circuits.
-            transpiled_circuits = {
-                k:  qiskit.transpile(circuit, backend, optimization_level=0)
-                for k, circuit in circuits.items()
-            }
-        
-            # Run task.
-            print(f"Trial {trial} out of {num_trials} for task of {charlie_size=}")
-            tasks[trial][charlie_size] = backend.run(
-                list(transpiled_circuits.values()),
-                shots=shots,
-                verbatim=True,
-            )
-            print(f"Task with task ID: {tasks[trial][charlie_size].job_id()}\n")
+    friend_sizes = []
+    for charlie_size in charlie_sizes:
+        for debbie_size in debbie_sizes:
+            for trial in range(1, num_trials + 1):
+                circuits = {}
+            
+                # Create circuits for each EWFS setting.
+                # Note, we remove barriers in the Qiskit circuit.
+                for alice_setting, bob_setting in settings:
+                    circuit = EWFS(
+                        alice_setting=alice_setting,
+                        bob_setting=bob_setting,
+                        strategy=strategy,
+                        charlie_size=charlie_size,
+                        debbie_size=debbie_size).circuit()
+                    circuits[(alice_setting, bob_setting)] = circuit
+            
+                # Use backend to transpile circuits.
+                transpiled_circuits = {
+                    k:  qiskit.transpile(circuit, backend, optimization_level=0)
+                    for k, circuit in circuits.items()
+                }
+            
+                # Run task.
+                friend_size = f"{charlie_size}_{debbie_size}"
+                friend_sizes.append(friend_size)
+                print(f"Trial {trial} out of {num_trials} for task of {friend_size}")
+                tasks[trial][friend_size] = backend.run(
+                    list(transpiled_circuits.values()),
+                    shots=shots,
+                    verbatim=True,
+                )
+                print(f"Task with task ID: {tasks[trial][friend_size].job_id()}\n")
 
     results = {}
     post_processed_results = {
@@ -62,7 +67,7 @@ def run_experiment(
         for fs in friend_sizes
     }
     for trial in tasks:
-        for charlie_size, task in tasks[trial].items():
+        for friend_size, task in tasks[trial].items():
             print(f"Processing trial {trial} for task with task ID: {task.job_id()}")
 
             result = task.result()
@@ -75,14 +80,15 @@ def run_experiment(
             violations = compute_violations(
                 results=results,
                 charlie_size=charlie_size,
-                debbie_size=1,
+                debbie_size=debbie_size,
                 strategy=strategy,
                 verbose=True
             )
             print(f"Violations: {violations}\n")
         
             for key in violations:
-                post_processed_results[charlie_size][key].append(violations[key])
+                post_processed_results[friend_size][key].append(violations[key])
+            print(f"Post-processed results: {post_processed_results}\n")
 
 
 if __name__ == "__main__":
