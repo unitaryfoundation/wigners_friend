1a5bf73dc5df8625b4df4af21f077d1de79bcca5
Will Zeng <zeng.will@gmail.com>
2025-01-06 21:36:11 -0500

    simplify noise models and update demo

--- ewfs/noise_models.py
+++ ewfs/noise_models.py
@@ -23,43 +23,27 @@ def get_n_qubit_gateset(circ: QuantumCircuit, num_qubits: int) -> set[str]:
     }
 
 
-def depolarizing_model(circ: QuantumCircuit, single_qubit_error_rate=0.01, two_qubit_error_rate=0.03) -> NoiseModel:
+def depolarizing_model(circ: QuantumCircuit, 
+                       single_qubit_error_rate: float=0.01, 
+                       two_qubit_error_rate:float=0.03, 
+                       meas_error_rate : float | None = None) -> NoiseModel:
     """Depolarizing noise model with error rates applied to the single and two-qubit gates of the circuit.
     Args:
-        circ: Quantum circuit.
+        circ: Quantum circuit to apply the model to.
+        single_qubit_error_rate: Error rate for a single qubit gate.
+        two_qubit_error_rate: Error rate for a two qubit gate.
+        meas_error_rate (optional): Bit flip error for a measurement.
     Returns:
         Depolarizing noise model.
     """
     single_qubit_gates = get_n_qubit_gateset(circ, num_qubits=1)
     two_qubit_gates = get_n_qubit_gateset(circ, num_qubits=2)
-
+    
     noise_model = NoiseModel()
+    if meas_error_rate:
+        error_meas = pauli_error([("X", meas_error_rate), ("I", 1 - meas_error_rate)])
+        noise_model.add_all_qubit_quantum_error(error_meas, "measure")
     noise_model.add_all_qubit_quantum_error(depolarizing_error(single_qubit_error_rate, 1), list(single_qubit_gates))
     noise_model.add_all_qubit_quantum_error(depolarizing_error(two_qubit_error_rate, 2), list(two_qubit_gates))
+
     return noise_model
-
-
-def bitflip_model(p: float) -> NoiseModel:
-    """Bitflip noise model with majority vote approach.
-
-    Args:
-        p: Probability to flip.
-    Returns:
-        Bit-flip noise model.
-    """
-    # Example error probabilities.
-    p_meas = p
-    p_gate1 = p
-
-    # QuantumError objects.
-    error_meas = pauli_error([("X", p_meas), ("I", 1 - p_meas)])
-    error_gate1 = pauli_error([("X", p_gate1), ("I", 1 - p_gate1)])
-    error_gate2 = error_gate1.tensor(error_gate1)
-
-    # Add errors to noise model.
-    noise_bit_flip = NoiseModel()
-    noise_bit_flip.add_all_qubit_quantum_error(error_meas, "measure")
-    noise_bit_flip.add_all_qubit_quantum_error(error_gate1, ["u1", "u2", "u3"])
-    noise_bit_flip.add_all_qubit_quantum_error(error_gate2, ["cx"])
-
-    return noise_bit_flip
