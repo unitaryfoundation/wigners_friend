4d4f0f206b7fbaf0481cf1194725c3162282adfb
vprusso <vincentrusso1@gmail.com>
2025-03-02 13:58:20 -0500

    chore: add updated plots for paper from discussion.

--- ewfs/file_io.py
+++ ewfs/file_io.py
@@ -3,6 +3,8 @@
 import os
 import pickle
 import qiskit
+from ewfs.violations import compute_violations
+from ewfs.setting import PEEK, REVERSE_1, REVERSE_2
 
 
 def save_data(
@@ -36,3 +38,29 @@ def save_data(
     print(f"Writing data to: {save_path}")
     with open(save_path, "wb") as handle:
         pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def load_experiments(
+    machine_name: str,
+    friend_sizes: list[int],
+    num_trials: int,
+    shots: int,
+    data_path: str,
+    strategy: str,
+) -> dict:
+    """Load experiments from multiple files."""
+    all_results: dict = {fs: {inequality: [] for inequality in ["semi_brukner"]} for fs in friend_sizes}
+
+    for friend_size in friend_sizes:
+        for trial in range(1, num_trials + 1):
+            with open(
+                os.path.join(data_path, f"{machine_name}_qubits_{friend_size}_trial_{trial}_shots_{shots}.pickle"), "rb"
+            ) as file:
+                results = pickle.load(file)
+            mapping = {1: PEEK, 2: REVERSE_1, 3: REVERSE_2}
+            results = {(mapping[k1], mapping[k2]): v for (k1, k2), v in results.items()}
+
+            violations = compute_violations(results=results, charlie_size=friend_size, debbie_size=1, strategy=strategy)
+            for key in violations:
+                all_results[friend_size][key].append(violations[key])
+    return all_results
